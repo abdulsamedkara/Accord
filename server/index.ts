a6b9a2520173932c403bbdecffb1cabecfeb4987
Abdül Samed Kara <abdulsamed_kara21@erdogan.edu.tr>
2026-02-09 18:18:45 +0300

    Update index.ts
    fix

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,17 @@ const dev = process.env.NODE_ENV !== "production";
 const hostname = "0.0.0.0";
 const port = parseInt(process.env.PORT || "3000", 10);
 
+// DEBUG LOGGING
+console.log("----------------------------------------");
+console.log("Current Working Directory:", process.cwd());
+console.log("Directory Contents:", require("fs").readdirSync(process.cwd()));
+try {
+    console.log(".next Directory Contents:", require("fs").readdirSync(require("path").join(process.cwd(), ".next")));
+} catch (e) {
+    console.log("Could not read .next directory:", e.message);
+}
+console.log("----------------------------------------");
+
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
